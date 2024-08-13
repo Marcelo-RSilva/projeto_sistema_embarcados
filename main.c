58bd3caf8d36ad8b5608157183a45361e877e929
Antonio Roberto <69351694+AntonioJunior2222@users.noreply.github.com>
2024-08-13 10:48:50 -0300

    Att Quartenion e Angulos Euler

--- main.c
+++ main.c
@@ -217,16 +217,27 @@ void app_main() {
     while (1) {
         // Lê os dados de aceleração e giroscópio do IMU
         if (imu_read_data(&imu_data) == ESP_OK) {
-            // Imprime os valores de aceleração e rotação
-            printf("\nAcceleration [X, Y, Z]: %f, %f, %f\n\n | Gyroscope [X, Y, Z]: %f, %f, %f\n",
+            // Calcula o quaternion com base nos dados lidos
+            imu_calculate_quaternion(&imu_data, &quaternion);
+
+            // Calcula os ângulos de Euler a partir do quaternion
+            imu_calculate_euler_angles(&quaternion, &euler);
+
+            // Imprime os valores de aceleração, rotação e ângulos de Euler
+            printf("\nAcelerômetro [X, Y, Z]: %f, %f, %f\nGiroscopio [X, Y, Z]: %f, %f, %f\n",
                    imu_data.accel.x, imu_data.accel.y, imu_data.accel.z,
                    imu_data.gyro.x, imu_data.gyro.y, imu_data.gyro.z);
-        } else {
-            printf("Failed to read IMU data.\n");
-        }
 
-        // Delay de 1 segundo entre as leituras
-        vTaskDelay(pdMS_TO_TICKS(1000));
+            printf("\nQuaternion [W, X, Y, Z]: %f, %f, %f, %f\n",
+                   quaternion.w, quaternion.x, quaternion.y, quaternion.z);
+
+            printf("\nAngulos de Euler [Roll, Pitch, Yaw]: %f, %f, %f\n",
+                   euler.roll, euler.pitch, euler.yaw);
+        } else {
+            printf("Falha ao ler os dados IMU.\n");
+        }
+      // Delay de 1 segundo
+      vTaskDelay(pdMS_TO_TICKS(3000));
     }
 
     imu_deinit();  // Desinstala o driver I2C e desinicializa o IMU
